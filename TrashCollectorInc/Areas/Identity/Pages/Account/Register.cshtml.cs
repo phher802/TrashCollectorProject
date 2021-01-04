11c287a0e0e6b7bf4c15c43ae0138e67e0829300
phher802 <pher024@gmail.com>
2021-01-03 23:54:47 -0600

    Add Rople property to InputModel class and SelectList Roles property to RegisterModel class

--- TrashCollectorInc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TrashCollectorInc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
@@ -41,6 +42,7 @@ namespace TrashCollectorInc.Areas.Identity.Pages.Account
 
         [BindProperty]
         public InputModel Input { get; set; }
+        public SelectList Roles { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -63,6 +65,9 @@ namespace TrashCollectorInc.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Role { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
