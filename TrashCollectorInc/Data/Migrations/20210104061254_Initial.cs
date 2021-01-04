72936199973b5eee0bfd333213818eb6e5c0754c
phher802 <pher024@gmail.com>
2021-01-04 00:20:52 -0600

    Create Customer model and its properties

---
+++ TrashCollectorInc/Data/Migrations/20210104061254_Initial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollectorInc.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "321791b5-23b0-448f-aa51-37b8dc52dbd6", "0e974e26-1124-454e-ab39-6e03ba8621b0", "ADMIN", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "321791b5-23b0-448f-aa51-37b8dc52dbd6");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ TrashCollectorInc/Models/Customer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TrashCollectorInc.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is a Required Input")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is a Required Input")]
+        public string LastName { get; set; }
+
+
+        [Display(Name = "Street Address")]
+        [Required(ErrorMessage = "Street Address is a Required Input")]
+        public string StreetAddress { get; set; }
+
+        //include city in a dropdown list auto populated by state?
+        [Display(Name = "City")]
+        public string CityName { get; set; }
+
+        //include in a dropdown list? 
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        [Required(ErrorMessage = "Zip Code is a Required Input")]
+        public String ZipCode { get; set; }
+
+        [Display(Name = "Primary Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Start Pickup Date")]
+        [DataType(DataType.Date)]
+        public DateTime? StartPickupDate { get; set; }
+
+        [Display(Name = "Suspend Pickup Date")]
+        [DataType(DataType.Date)]
+        public DateTime? SuspendPickup { get; set; }
+
+        [Display(Name = "Weekly Pickup Day")]
+        [NotMapped]
+        public IEnumerable<SelectListItem> WeeklyPickupDay { get; set; }
+
+        [Display(Name = "One-time Pickup Request")]
+        [DataType(DataType.Date)]
+        public DateTime? OneTimePickupDateRequest { get; set; }
+
+        [Display(Name = "Monthly Charge")]
+        public double MonthlyCharge { get; set; }
+
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+    }
+}
