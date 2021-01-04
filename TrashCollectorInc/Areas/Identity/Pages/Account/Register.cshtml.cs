93e56cf31ee7f6b3108818a539bdf275f704d956
phher802 <pher024@gmail.com>
2021-01-03 23:59:58 -0600

    Edit methods in OnGetAsync and OnPostAsync to initialize and add user role relationship to database

--- TrashCollectorInc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TrashCollectorInc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,6 +74,8 @@ namespace TrashCollectorInc.Areas.Identity.Pages.Account
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var roles = _roleManager.Roles;
+            Roles = new SelectList(roles, "Name", "Name");
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -86,6 +88,10 @@ namespace TrashCollectorInc.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    if(await _roleManager.RoleExistsAsync(Input.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
