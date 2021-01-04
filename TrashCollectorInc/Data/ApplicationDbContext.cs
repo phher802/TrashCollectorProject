90cb4f5cfdeb1934374e73e5b27b1e0ec4a88da4
phher802 <pher024@gmail.com>
2021-01-04 09:22:57 -0600

    Add customer and employee Dbset properties

--- TrashCollectorInc/Data/ApplicationDbContext.cs
+++ TrashCollectorInc/Data/ApplicationDbContext.cs
@@ -4,11 +4,15 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrashCollectorInc.Models;
 
 namespace TrashCollectorInc.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
