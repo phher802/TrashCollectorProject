66489c19512488094e7c2920054845d5d7d7489f
phher802 <pher024@gmail.com>
2021-01-04 00:12:11 -0600

    Seed data into database

--- TrashCollectorInc/Data/ApplicationDbContext.cs
+++ TrashCollectorInc/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,13 @@ namespace TrashCollectorInc.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+                .HasData(new IdentityRole { Name = "ADMIN", NormalizedName = "ADMIN" });
         }
     }
 }
